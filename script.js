a83ecadb472b7bc217b5f824b0f848b4dbfb456c
JakubStranianek <j.stranianek@gmail.com>
2022-02-06 16:29:31 +0100

    done but without transitions

--- script.js
+++ script.js
@@ -1,61 +1,112 @@
+
 class App extends React. Component {
     constructor(props) {
       super(props)
     
+      this.input = React.createRef();
       this.state = {
-          dude: "Marceline the Vampire",
-          characters: [
-                {
-                  "id": 1,
-                  "who": "Finn the Human",
-                  "wat": "A silly kid who wants to become a great hero one day.",
-                  "cool": 9
-                },
-                {
-                  "id": 2,
-                  "who": "Jake the Dog",
-                  "wat": "Finn's best friend is a wise, old dog with a big heart.",
-                  "cool": 42
-                },
-                {
-                  "id": 3,
-                  "who": "Ice King",
-                  "wat": "Armed with a magic crown and an icy heart.",
-                  "cool": 0
-                },
-                {
-                  "id": 4,
-                  "who": "Princess Bubblegum",
-                  "wat": "A millionaire nerd enthusiast.",
-                  "cool": 24
-                },
-                {
-                  "id": 5,
-                  "who": "Marcy the Vampire",
-                  "wat": "Marceline is a wild rocker girl.",
-                  "cool": 9
-                }
-              ]
+          newWho: "",
+          newWat: "",
+          characters: []
       }
     }
     
-    handleChange = event => {
+    handleWho = event => {
         this.setState({
-            dude: event.target.value
+            newWho: event.target.value
         })
     }
-    
-    render() {
-    const dudes = this.state.characters.map(dude => <li key={dude.id}>{dude.who}</li>);
 
+    handleWat = event => {
+      this.setState({
+          newWat: event.target.value
+      })
+  }
+
+    handleSubmit = event => {
+      if (event.key === "Enter" && this.state.newWho && this.state.newWat) {
+         this.setState(state => {
+        const newDude = {
+          id: Math.max(...this.state.characters.map(d => d.id)) + 1,
+          who: this.state.newWho,
+          wat: this.state.newWat,
+          cool: Math.floor(Math.random() * 100) + 1
+        }
+
+        
+        return {characters: [...state.characters, newDude]}
+      })
+      this.resetForm()
+    }
+  }
+    
+  handleChangeCool = dude => event => {
+    const cool = +event.target.value
+    
+    this.setState(state => {
+      return {
+          characters: state.characters.map(item =>
+              item === dude ? { ...item, cool } : item
+          )
+      }
+  })
+    
+  }
+
+
+  removeDude = dude => {
+      this.setState(state => {
+        return {
+          characters: state.characters.filter(item => item !== dude)
+        }  
+    })
+  }
+
+  
+  resetForm = () => {
+
+   this.setState({
+    newWho: "",
+    newWat: ""
+   })
+
+    this.input.current.focus()
+  }
+
+    listOfDudes = () => {
+      return this.state.characters.map(dude => (
+
+          <li key={dude.id} className="dude">
+        <a className="ctrl" onClick={() => this.removeDude(dude)}>x</a>
+        
+        <article className={dude.cool < 10 ? "faded" : dude.cool > 50 ? "gold" : ""}>
+        {dude.who}
+        <span>{dude.wat}</span>
+        </article>
+
+        <input className="ctrl" type="number" onChange={this.handleChangeCool(dude)} value={dude.cool}></input>
+        
+        </li>
+        )  
+      )
+    }
+
+    componentDidMount = () => {
+      fetch("http://myjson.dit.upm.es/api/bins/ck9p").then(res => res.json()).then(json => this.setState({characters: json}))
+    }
+
+    render() {
         return (
             <div>
-                <ul>{dudes}</ul>
+               <ul>{this.listOfDudes()}</ul>
 
-                <form className="add-new">
-                    <input type="text" value={this.state.dude} onChange={this.handleChange} />
+                <form className="add-new" onKeyPress={this.handleSubmit}>
+                    <input autoFocus type="text" value={this.state.newWho} onChange={this.handleWho} ref={this.input}/>
+                    <input type="text" value={this.state.newWat} onChange={this.handleWat}/>
                 </form>
-                <p className="preview">{this.state.dude}</p>
+                <p className="preview">{this.state.newWho}</p>
+                <br/>
+                <small className="preview">{this.state.newWat}</small>
             </div>
         )    
     }
